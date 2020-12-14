ce406dc928100464560821bbf30b4e4734ca1162
Brian Clayton <finalb@projectfinalboss.com>
2020-12-13 21:20:13 -0500

    update

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ const server = require("./server") ;
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
-
-    console.log(`API running on port: ${PORT}`);
-
-})
+    console.log(`\n** API running on port: ${PORT} **\n`);
+});
