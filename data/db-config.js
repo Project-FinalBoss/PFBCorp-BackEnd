9af471af8e496a2dd320e73688ec16bd6fffbf15
Brian Clayton <finalb@projectfinalboss.com>
2021-09-27 10:38:16 -0400

    Update

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,14 @@
-const knex = require('knex');
+require("dotenv").config()
 
-const config = require('../knexfile');
+const { Pool } = require('pg')
 
-module.exports = knex(config.development);
+const isProduction = process.env.NODE_ENV === "production"
 
+const connectionString =   `postgressql://${process.env.DB_USER}:${process.env.DB_PASSWORD}:${process.env.DB_HOST}:${process.env.DB_PORT}:${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+
+})
+
+module.exports = { pool }
