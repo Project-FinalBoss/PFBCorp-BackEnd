419f26573c0cec20f7af7b1db026b5bb585de40c
Brian Clayton <finalb@projectfinalboss.com>
2021-10-01 17:05:32 -0400

    update

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-// const { pool } = require('./data/db-config')
 const express = require("express")
 const helmet = require('helmet')
 const app = express()
-const { Pool } = require('pg')
 const UserRouter = require('./users/user-router')
 
 
 
 const PORT = process.env.DB_PORT || 9000
-const USER = process.env.DB_USER
-const PASSWORD = process.env.DB_PASSWORD
-const HOST = process.env.DB_HOST
-const DB =  process.env.DB_DATABASE
 
 
-const pool = new Pool({USER, HOST, DB, PASSWORD, PORT })
 
 
 app.get('/', (req, res) =>{
@@ -22,11 +15,6 @@ app.get('/', (req, res) =>{
 })
 
 
-
-    pool.query('SELECT * FROM register', (err, res) => {
-        if (err) return console.log(err)
-        console.log(res.rows)
-    })
 
 
 
@@ -37,4 +25,4 @@ app.listen(PORT, () => {
 
 
 app.use(helmet())
-app.use('/users/user-router', UserRouter)
+app.use('/users', UserRouter)
